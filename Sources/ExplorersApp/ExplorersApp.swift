922afa6c2287a875bca2310a2877853b32dc2088
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-05-28 09:12:02 +0800

    implement explorers app

--- Sources/ExplorersApp/ExplorersApp.swift
+++ Sources/ExplorersApp/ExplorersApp.swift
@@ -1,6 +1,36 @@
-public struct ExplorersApp {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+@available(iOS 16.0, *)
+@available(macOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+public struct ExplorersApp<T: View>: View {
+    
+    @ViewBuilder
+    var contents: (() -> T)
+    
+    private let width = 1080.0
+    private let height = 810.0
+    
+    public var body: some View {
+        GeometryReader { reader in
+            Rectangle()
+                .fill(.black)
+            
+            let scale = min(reader.size.width / width, reader.size.height / height)
+            
+            contents()
+                .frame(width: width, height: height)
+                .background(Color.white.shadow(color: .black.opacity(0.3), radius: 8))
+                .scaleEffect(scale)
+                .frame(width: reader.size.width, height: reader.size.height)
+                .onAppear {
+                    print(reader.size.width, reader.size.height)
+                }
+                .preferredColorScheme(.light)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .persistentSystemOverlays(.hidden)
+        .statusBarHidden()
     }
 }
